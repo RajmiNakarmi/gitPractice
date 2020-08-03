8f67d77e86835847a96ed99c6682863ff85ea00e
rajmi <rajminakarmi26@gmail.com>
2020-08-02 22:01:49 -0400

    update login.java

--- src/login.java
+++ src/login.java
@@ -2,5 +2,6 @@ public class login {
     public static void main(String[] args) {
         System.out.println("welcome to git");
         System.out.println("first practice");
+        //checking changes
     }
 }
