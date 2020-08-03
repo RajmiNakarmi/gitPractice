23c4e813eb9d274c86dbde96bbea3e42216b9283
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-08-02 22:25:53 -0400

    Update login.java

--- src/login.java
+++ src/login.java
@@ -3,5 +3,6 @@ public class login {
         System.out.println("welcome to git");
         System.out.println("first practice");
         //checking changes
+//everything is fine.. push from remote to local rep.
     }
 }
