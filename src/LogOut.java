d6f81bb3d7ec3d7ddd26ee9ea3131ef2c03e8f95
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-08-02 22:30:15 -0400

    Update LogOut.java

--- src/LogOut.java
+++ src/LogOut.java
@@ -2,5 +2,7 @@ public class LogOut {
     // to see in gitHub
     public static void main(String[] args) {
         System.out.println("interesting! learning.");
+          System.out.println(" more practice need Rajmi");
+        
     }
 }
